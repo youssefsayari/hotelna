9ba95857efea51952ea1733cc28e6795e43c5011
Ghassen <ghassen.benaissa@esprit.tn>
2025-02-16 20:49:25 +0100

    Main

---
+++ ApiGateway/src/main/java/com/apigateway/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,25 @@
+package com.apigateway.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+    @Bean
+    public RouteLocator gatewayRoot(RouteLocatorBuilder builder)
+    {
+        return builder.routes()
+                .route("restaurants",r -> r.path("/restaurants/**").uri("lb://Restaurants"))
+                .build();
+    }
+
+}
