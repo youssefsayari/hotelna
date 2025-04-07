444806c0d5fd067758d43f0eba7ca118979abe9f
Ghassen <123995471+ghassenbenaissaa@users.noreply.github.com>
2025-04-07 23:14:35 +0100

    Fix

--- src/main/java/com/hotelna/restaurants/Entity/Restaurant.java
+++ src/main/java/com/hotelna/restaurants/Entity/Restaurant.java
@@ -4,34 +4,49 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+import java.sql.Time;
+
 @Entity
 public class Restaurant {
+
     @Id
     @GeneratedValue
     private int id;
     private String name;
+    private String descritpion;
     private String statut;
     private String typeRestaurant;
-    private String menu;
+    private Time openTime;
+    private Time closeTime;
 
     public Restaurant() {
     }
 
-    public Restaurant(int id, String name, String statut, String typeRestaurant, String menu) {
+    public Restaurant(String name, String descritpion, String statut, String typeRestaurant, Time openTime, Time closeTime) {
+        this.name = name;
+        this.descritpion = descritpion;
+        this.statut = statut;
+        this.typeRestaurant = typeRestaurant;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+    }
+
+    public Restaurant(int id, String name, String descritpion, String statut, String typeRestaurant, Time openTime, Time closeTime) {
         this.id = id;
         this.name = name;
+        this.descritpion = descritpion;
         this.statut = statut;
         this.typeRestaurant = typeRestaurant;
-        this.menu = menu;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
     }
-    public Restaurant( String name, String statut, String typeRestaurant, String menu) {
-        this.name = name;
-        this.statut = statut;
-        this.typeRestaurant = typeRestaurant;
-        this.menu = menu;
-    }
+
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -40,6 +55,14 @@ public class Restaurant {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescritpion() {
+        return descritpion;
+    }
+
+    public void setDescritpion(String descritpion) {
+        this.descritpion = descritpion;
     }
 
     public String getStatut() {
@@ -58,13 +81,19 @@ public class Restaurant {
         this.typeRestaurant = typeRestaurant;
     }
 
-    public String getMenu() {
-        return menu;
+    public Time getOpenTime() {
+        return openTime;
     }
 
-    public void setMenu(String menu) {
-        this.menu = menu;
+    public void setOpenTime(Time openTime) {
+        this.openTime = openTime;
     }
 
+    public Time getCloseTime() {
+        return closeTime;
+    }
 
+    public void setCloseTime(Time closeTime) {
+        this.closeTime = closeTime;
+    }
 }
