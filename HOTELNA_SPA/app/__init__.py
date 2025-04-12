9cbd3e751608bd94eeed7b99e241f6efd891c67f
amenk21 <amenallah.kthiri@esprit.tn>
2025-04-12 18:12:42 +0100

    s

--- HOTELNA_SPA/app/__init__.py
+++ HOTELNA_SPA/app/__init__.py
@@ -1,6 +1,7 @@
 #init.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
 
 # Global SQLAlchemy instance
@@ -10,6 +11,8 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
+
+    CORS(app)
 
     db.init_app(app)
 
