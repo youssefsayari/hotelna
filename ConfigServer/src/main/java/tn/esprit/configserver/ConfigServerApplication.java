3ea30f1f8b38d70bbab63bd8b7f03bd5b408a12f
amenk21 <amenallah.kthiri@esprit.tn>
2025-04-13 21:39:21 +0100

    CONFIG_SERVER

--- ConfigServer/src/main/java/tn/esprit/configserver/ConfigServerApplication.java
+++ ConfigServer/src/main/java/tn/esprit/configserver/ConfigServerApplication.java
@@ -1,18 +1,17 @@
-package tn.esprit.innoxpert;
+package tn.esprit.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-
-public class Innoxpert {
+@EnableConfigServer
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Innoxpert.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
